21a1dcc0148eeafb8daba74b55a8c45b39d79d5a
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-26 18:37:37 +0000

    Following the course implementation I grabbed all necessary elements from the dom

--- script2.js
+++ script2.js
@@ -1,77 +1,53 @@
-const currencyOne = document.getElementById("currency-one");
-const currencyTwo = document.getElementById("currency-two");
+const currencyEl_one = document.getElementById("currency-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_one = document.getElementById("amount-one");
+const amountEl_two = document.getElementById("amount-two");
+const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
-const amountOne = document.getElementById("amount-one");
-const amountTwo = document.getElementById("amount-two");
-const rate = document.getElementById("rate");
 
-let dataFromFirstFetch = {};
+//code below was my implementation which is a little convoluted and had problems implementing swap functionality
+// const currencyOne = document.getElementById("currency-one");
+// const currencyTwo = document.getElementById("currency-two");
+// const swap = document.getElementById("swap");
+// const amountOne = document.getElementById("amount-one");
+// const amountTwo = document.getElementById("amount-two");
+// const rate = document.getElementById("rate");
 
-//HELPER FUNCTIONS
-function firstFunction() {
-  fetch(
-    `https://v6.exchangerate-api.com/v6/b46651af6394c073d2647b48/latest/${currencyOne.value}`
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const baseRate = data.base_code;
-      const baseRateValue = data.conversion_rates[baseRate];
-      amountOne.value = baseRateValue;
-      dataFromFirstFetch = data;
-      secondFunction();
-    });
-}
-
-const secondFunction = () => {
-  //set up currencyToFind variable
-  //set up rateValue to display that will match the value of the foud currency
-
-  const currencyToFind = currencyTwo.value;
-
-  const rateValue = dataFromFirstFetch.conversion_rates[currencyToFind];
-  if (rateValue) {
-    amountTwo.value = rateValue;
-  } else {
-    console.log("rate not found for selected currency");
-  }
-};
-
-const thirdFunction = () => {};
-
-//EVENT LISTENERS
-currencyOne.addEventListener("change", firstFunction);
-currencyTwo.addEventListener("change", secondFunction);
-swap.addEventListener("click", thirdFunction);
+// let dataFromFirstFetch = {};
+// let dataFromSwapFetch = {};
 
 // //HELPER FUNCTIONS
 // function firstFunction() {
-//     fetch(`https://v6.exchangerate-api.com/v6/b46651af6394c073d2647b48/latest/${currencyOne.value}`)
-//       .then(response => response.json())
-//       .then(data => {
-//         dataFromFirstFetch = data; // Store data in the global variable
-//         updateUI(); // Update UI elements based on fetched data
-//       });
+//   fetch(
+//     `https://v6.exchangerate-api.com/v6/b46651af6394c073d2647b48/latest/${currencyOne.value}`
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       const baseRate = data.base_code;
+//       const baseRateValue = data.conversion_rates[baseRate];
+//       amountOne.value = baseRateValue;
+//       dataFromFirstFetch = data;
+//       console.log(data);
+//       secondFunction();
+//       rate.innerHTML = `${baseRateValue} ${baseRate} = ${amountTwo.value} ${currencyTwo.value}`;
+//     });
+// }
+
+// const thirdFunction = () => {};
+
+// const secondFunction = () => {
+//   const currencyToFind = currencyTwo.value;
+//   const rateValue = dataFromFirstFetch.conversion_rates[currencyToFind];
+//   if (rateValue) {
+//     amountTwo.value = rateValue;
+//   } else {
+//     console.log("rate not found for selected currency");
 //   }
+// };
 
-//   function updateUI() {
-//     // Assuming you want to update amountOne with the base rate value
-//     const baseRate = dataFromFirstFetch.base_code;
-//     const baseRateValue = dataFromFirstFetch.conversion_rates[baseRate];
-//     amountOne.value = baseRateValue;
-
-//     // Call secondFunction to update amountTwo
-//     secondFunction();
-//   }
-
-//   function secondFunction() {
-//     const currencyToFind = currencyTwo.value;
-//     const currencyValue = dataFromFirstFetch.conversion_rates[currencyToFind];
-//     amountTwo.value = currencyValue;
-//   }
-
-//   //EVENT LISTENERS
-//   currencyOne.addEventListener("change", firstFunction);
-//   currencyTwo.addEventListener("change", secondFunction);
-//   // ... other event listeners
+// //EVENT LISTENERS
+// currencyOne.addEventListener("change", firstFunction);
+// currencyTwo.addEventListener("change", secondFunction);
+// swap.addEventListener("click", thirdFunction);
